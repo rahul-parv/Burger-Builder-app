53efecec883bce2a451d7b31a46f3a2dae73f044
rahul-parv <rahuljatparv@yahoo.com>
2019-06-07 16:04:11 +0530

    check validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -93,21 +93,35 @@ class ContactData extends Component {
         isFormValid: false
     }
 
-
     checkValidity = (value, rule) => {
-        let isValide = true;
-
-        if (rule && rule.required) {
-            isValide = value.trim() !== '' && isValide
+        let isValid = true;
+        if (!rules) {
+            return true;
         }
-        if (rule && rule.minLength) {
-            isValide = value.length >= rule.minLength && isValide
-        }
-        if (rule && rule.maxLength) {
-            isValide = value.length <= rule.maxLength && isValide
+        
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
         }
 
-        return isValide; 
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid
+        }
+
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        return isValid;
     }
 
     inputChangeHandler = (event, elemIdentifier) => {
